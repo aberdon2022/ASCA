4c4fd2a23961c905cd61531cb2c8241c3fd21ce3
aberdon2022 <alejandrob25082003@gmail.com>
2025-05-02 12:59:37 +0200

    First Commit

---
+++ src/main/java/org/chatproject/ascp/AscaApplication.java
@@ -0,0 +1,13 @@
+package org.chatproject.ascp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chatproject/ascp/AscpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chatproject.ascp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
